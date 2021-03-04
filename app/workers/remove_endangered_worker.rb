a4feeebe7a7ec1c2f50677a6d7fe87fa54947fb0
Reginaldo Junior <reginaldo.junior@vagas.com.br>
2021-03-04 13:45:08 -0300

    Adiciona o rubocop para facilitar padronização de código

--- app/workers/remove_endangered_worker.rb
+++ app/workers/remove_endangered_worker.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveEndangeredWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
@@ -5,5 +7,4 @@ class RemoveEndangeredWorker
   def perform
     Endangered.destroy_all
   end
-
 end
